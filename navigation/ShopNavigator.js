3cfe49683742749a82f03d6a5c610714e37366cb
Amar Maruf <amarmaruf2403@gmail.com>
2021-04-24 02:04:16 +0700

    menambahkankan menu samping dan mengedit File OrdersScreen (SideDrawer & the Orders Screens)

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,11 +1,28 @@
+import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import {Platform} from 'react-native';
 
 import Colors from '../constants/Color';
 import ProductsOverviewScreen from '../screens/shop/ProductOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import OrdersScreen from '../screens/shop/OrdersScreen';
 import CartScreen from '../screens/shop/CartScreen';
+import { Ionicons } from '@expo/vector-icons';
+
+const defaultNavOptions = {
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+    },
+    headerTitleStyle: {
+      fontFamily: 'open-sans-bold'
+    },
+    headerBackTitle: {
+      fontFamily: 'open-sans-bold'
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+}
 
 const ProductNavigators = createStackNavigator (
     {
@@ -14,19 +31,45 @@ const ProductNavigators = createStackNavigator (
         Cart: CartScreen
     },
     {
-      defaultNavigationOptions: {
-        headerStyle: {
-          backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
-        },
-        headerTitleStyle: {
-          fontFamily: 'open-sans-bold'
-        },
-        headerBackTitle: {
-          fontFamily: 'open-sans-bold'
-        },
-        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
-      }
+      navigationOptions: {
+        drawerIcon: drawerConfig => (
+        <Ionicons 
+        name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'} 
+        size={23}
+        color={drawerConfig.tintColor}
+        />
+      )
+      },
+      defaultNavigationOptions: defaultNavOptions
     }
-    );
+    ); 
 
-export default createAppContainer(ProductNavigators);
+const OrdersNavigator = createStackNavigator (
+  {
+    Orders: OrdersScreen
+  }, {
+    navigationOptions: {
+      drawerIcon: drawerConfig => (
+      <Ionicons 
+      name={Platform.OS === 'android' ? 'md-list' : 'ios-list'} 
+      size={23}
+      color={drawerConfig.tintColor}
+      />
+    )
+    },
+    defaultNavigationOptions: defaultNavOptions
+  }
+);
+
+const ShopNavigator = createDrawerNavigator(
+  {
+    Products: ProductNavigators,
+    Orders: OrdersNavigator
+  }, {
+    contentOptions: {
+      activeTintColor: Colors.primary
+    }
+  }
+);
+
+export default createAppContainer(ShopNavigator);
