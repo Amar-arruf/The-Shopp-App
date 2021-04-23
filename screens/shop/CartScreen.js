1d7b5aefe6b53f76b7f5627e9ab4d5876ca3b659
Amar Maruf <amarmaruf2403@gmail.com>
2021-04-23 11:43:39 +0700

    menghapus item cart (Adding Redux logic to Delete Items)

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View, Text, FlatList , Button, StyleSheet } from 'react-native';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 
 import Colors from '../../constants/Color';
 import CartItem from '../../components/shop/CartItem';
+import * as cartActions from '../../store/actions/Cart';
 
 const CartScreen = props => {
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -18,37 +19,43 @@ const CartScreen = props => {
                 sum: state.cart.items[key].sum
             });
         }
-        return transformedCartItems;
+        return transformedCartItems.sort((a,b) =>
+         a.productId > b.productId ? 1 : -1
+         );
     });
-return (
-    <View style={styles.screen}>
-        <View style={styles.summary}>
-            <Text style={styles.summaryText}>
-                Total : <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
-            </Text>
-            <Button 
-                color={Colors.accent} 
-                title="Order Now" 
-                disabled={cartItem.length === 0}
-            />
+  
+    const dispatch = useDispatch();
+    return (
+        <View style={styles.screen}>
+            <View style={styles.summary}>
+                <Text style={styles.summaryText}>
+                    Total : <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text>
+                </Text>
+                <Button 
+                    color={Colors.accent} 
+                    title="Order Now" 
+                    disabled={cartItem.length === 0}
+                />
+            </View>
+            <View>
+                <FlatList 
+                    data={cartItem}
+                    keyExtractor={item => item.productId}
+                    renderItem={itemData => (
+                    <CartItem 
+                        quantity={itemData.item.quantity}
+                        title={itemData.item.productTitle}
+                        amount={itemData.item.sum}
+                        onRemove={()=>{
+                            dispatch(cartActions.removeFromCart(itemData.item.productId));
+                        }}
+                    />)
+                }
+                />
+            </View>
         </View>
-        <View>
-            <FlatList 
-                data={cartItem}
-                keyExtractor={item => item.productId}
-                renderItem={itemData => (
-                <CartItem 
-                    quantity={itemData.item.quantity}
-                    title={itemData.item.productTitle}
-                    amount={itemData.item.sum}
-                    onRemove={()=>{}}
-                />)
-            }
-            />
-        </View>
-    </View>
-)
-};
+    )
+    };
 
 const styles = StyleSheet.create({
     screen: {
